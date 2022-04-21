c1e87becae9f13fe281c039010b8e91ef9bb99dd
Noah Emke <noahremke@gmail.com>
2022-04-21 15:33:42 -0400

    Draw something to use as a player

--- MonoGame Snake Demo/Game1.cs
+++ MonoGame Snake Demo/Game1.cs
@@ -9,6 +9,14 @@ namespace MonoGame_Snake_Demo
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // An empty texture to draw plain colors
+        private Texture2D blankTexture;
+
+        // Keep track of where the player is
+        // vector to keep position as a float and rect to give the draw method
+        private Vector2 playerPos;
+        private Rectangle playerRect;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +26,9 @@ namespace MonoGame_Snake_Demo
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // Start the player at 30x30 at 100,100
+            playerPos = new Vector2(100, 100);
+            playerRect = new Rectangle(100, 100, 30, 30);
 
             base.Initialize();
         }
@@ -28,6 +38,10 @@ namespace MonoGame_Snake_Demo
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            // Make an empty texture that we can just tint to draw basic colors
+            blankTexture = new Texture2D(GraphicsDevice, 1, 1);
+            blankTexture.SetData(new Color[] { Color.White });
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,9 +56,15 @@ namespace MonoGame_Snake_Demo
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            // All the drawing must be within the begin and end calls of the spritebatch
+            _spriteBatch.Begin();
+
+            // Show the player as a blank square at the rectangle made earlier colored green
+            _spriteBatch.Draw(blankTexture, playerRect, Color.White);
+            
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
